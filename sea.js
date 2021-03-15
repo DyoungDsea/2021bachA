6ef676e3fd61a4f2730a781d42884e4653ade612
Young Elefiku <youngelefiku@gmail.com>
2021-03-15 12:16:36 +0100

    update

---
+++ sea.js
@@ -0,0 +1,43 @@
+var myName = "Young Elefiku";
+var myNamE = "Emmaunel";
+
+// alert(myNamE)
+
+// console.log(myName);
+// var _num = 1;
+// var __num = 2;
+var num1, num2, num3;
+
+num1 = "5";
+num2 = 10;
+num3 = "10";
+
+// var res = num2 !== num3;
+// var res = (num1== num2 && num2== num3);
+var res = !(num1== num2 || num2== num3);
+
+// var myName = "Young Elefiku"
+// var myName2 = "I am 23years old";
+
+// var res = myName +" "+ myName2;
+console.log(res);
+
+// console.log(typeof(num2));
+// console.log(--num2);
+// console.log(num1);
+// console.log(num1 += num2);
+// num1 = num1 + num2;
+
+// console.log(num1);
+
+
+
+
+
+
+//Data type
+
+// Number
+// String
+// Object
+// Array
