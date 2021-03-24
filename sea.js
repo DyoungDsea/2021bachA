3681974ee4fde5a01cd124027077dd9da97d3c8a
Young Elefiku <youngelefiku@gmail.com>
2021-03-24 10:50:16 +0100

    update

--- sea.js
+++ sea.js
@@ -1,43 +1,52 @@
-var myName = "Young Elefiku";
-var myNamE = "Emmaunel";
-
-// alert(myNamE)
-
-// console.log(myName);
-// var _num = 1;
-// var __num = 2;
-var num1, num2, num3;
-
-num1 = "5";
-num2 = 10;
-num3 = "10";
-
-// var res = num2 !== num3;
-// var res = (num1== num2 && num2== num3);
-var res = !(num1== num2 || num2== num3);
-
-// var myName = "Young Elefiku"
-// var myName2 = "I am 23years old";
-
-// var res = myName +" "+ myName2;
-console.log(res);
-
-// console.log(typeof(num2));
-// console.log(--num2);
-// console.log(num1);
-// console.log(num1 += num2);
-// num1 = num1 + num2;
-
-// console.log(num1);
 
 
 
 
+        var a=2, b=1, c=-1;
+        var insideSqrt = (Math.pow(b, 2) - 4 * a * c);
+        var x1, x2;
+
+        x1 = (- b + Math.sqrt(insideSqrt))/ 2 * a;
+        x2 = (- b - Math.sqrt(insideSqrt))/ 2 * a;
 
 
-//Data type
+        function solveQuad(a, b, c){
+            var insideSqrt = (Math.pow(b, 2) - 4 * a * c);
+            var x1, x2;
+            x1 = (- b + Math.sqrt(insideSqrt))/ 2 * a;            
+            x2 = (- b - Math.sqrt(insideSqrt))/ 2 * a;
+            return "The X1 = " + Math.floor(x1) + " X2 = " + Math.floor(x2);
+        }
 
-// Number
-// String
-// Object
-// Array
+        // solveQuad(2, 1, -1);
+
+        var cars = ["Venza", "camry", "Honda", "Range"];
+
+
+        cars.forEach(function (e){
+            console.log(e);
+        });
+  
+
+        cars.forEach(loopArray);
+
+        function loopArray(e){
+            console.log(e);
+        }
+
+        function deleteArray(arrayIndex){
+            delete arrayIndex;
+        }
+
+        deleteArray(ball[1]);
+
+        function myDetails(fullname, age){
+            console.log("My name is "+ fullname +" and my age is "+ age);
+        }
+        // solveQf(2, -1, 5);
+      myDetails("Young Elefiku", 23);
+      myDetails("Emmanuel", 33);
+      myDetails("Uche", 35);
+      myDetails("Ken", 31);
+
+        // sumForMe();
